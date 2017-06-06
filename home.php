49f97c9bae1bf3e9f104344e188ee11413bbf45a
Tyler Green <gree0209@stthomas.edu>
2017-06-05 21:58:15 -0500

    Added js and css for bootstrap and jquery, hopefully flyphicons work too, who knows.

--- home.php
+++ home.php
@@ -4,14 +4,20 @@
 <html>
 
     <head>
-        <title>Home</title>
-        <!-- Foundation Framework files-->
-        <script type="text/javascript" src="js/jquery-3.2.1.js"></script>
+        <title>Giant Airsoft Game</title>
+        <link rel="icon" type="image" href="img/favicon.ico">
+        <!-- Jquery and BootStrap js files -->
+        <script type="text/javascript" src="js/Vendor/jquery-3.2.1.js"></script>
+        <script type="text/javascript" src="js/Vendor/bootstrap.js"></script>
+        <!-- CSS for BootStrap -->
+        <link href="css/Vendor/bootstrap.css" rel="stylesheet">
+        <link href="css/Vendor/bootstrap-theme.css" rel="stylesheet">
 
     </head>
 
     <body>
-        <div></div>
+        <div>
+        </div>
     </body>
 
 </html>
