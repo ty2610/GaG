65fe4a066b4feff976f8fe5c2e2e82494a62c84c
Tyler Green <gree0209@stthomas.edu>
2017-06-08 23:26:13 -0500

    current iteration of stuff. Navbar, tca pic in background, fancy box with photo shopped pic of both teams.

--- home.php
+++ home.php
@@ -1,22 +1,73 @@
-<?php
-    echo "IT'S WORKING";
-?>
 <html>
 
     <head>
         <title>Giant Airsoft Game</title>
-        <!-- <link rel="shortcut icon" type="image" href="favicon.ico"> -->
         <!-- Jquery and BootStrap js files -->
         <script type="text/javascript" src="js/Vendor/jquery-3.2.1.js"></script>
         <script type="text/javascript" src="js/Vendor/bootstrap.js"></script>
         <!-- CSS for BootStrap -->
         <link href="css/Vendor/bootstrap.css" rel="stylesheet">
         <link href="css/Vendor/bootstrap-theme.css" rel="stylesheet">
+        <!-- local css and js -->
+        <link rel="stylesheet" href="css/home.css/">
 
     </head>
 
     <body>
-        <div>
+        <div class="container">
+
+            <!-- NavBar -->
+            <nav class="navbar navbar-inverse" style="line-height:2.5">
+                <a class="navbar-brand navlogo" href="http://www.twincitiesairsoft.com/index.html">
+                    <img src="img/twin_cities_airsoft_logo_small.png">
+                </a>
+                <ul class="nav nav-justified">
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">Details</a>
+                    </li>
+                    <li>
+                        <a href="#">Map</a>
+                    </li>
+                    <li>
+                        <a href="#">Rules</a>
+                    </li>
+                    <li>
+                        <a href="#">Registration</a>
+                    </li>
+                    <li>
+                        <a href="#">Pictures</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact</a>
+                    </li>
+                </ul>
+            </nav>
+            <!-- holder for everything but navbar -->
+            <div>
+                <!-- Clash section -->
+                <div id="Clash" class="row stripeBox loseMargin">
+                    <div class="col-md-3">
+                        <ul class="list-group" style="box-shadow:none">
+                            <li class="list-group-item listLoseBackground" style="border: none">
+                                <h3>Giant Airsoft Game VIII</h3>
+                            </li>
+                            <li class="list-group-item listLoseBackground" style="border: none">
+                                <h3>2261 130th Ave Baldwin, WI 54013</h3>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-md-6">
+                        <img src="img/WhiteSword.png">
+                    </div>
+                    <div class="col-md-3">
+                        <h3 style="color:white">October 7th-8th, 2017</h3>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </body>
 
